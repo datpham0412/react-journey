d989fc5119b9bd6d67374c8ce6c7a2683de4d718
datpham0412 <tiendat041202@gmail.com>
2024-08-15 10:27:42 +1000

    Event Handler: Button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useState, useRef } from "react";
+import React from "react";
+import Counter from "../src/rendering/EventHandler/Counter";
 
 function App() {
-  const inputRef = useRef(null);
-  const [inputValue, setInputValue] = useState("");
-
-  const focusInput = () => {
-    // Directly accessing the DOM element to set focus
-    inputRef.current.focus();
-  };
-
   return (
     <div>
-      <input
-        ref={inputRef}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={focusInput}>Focus the input</button>
+      <Counter />
     </div>
   );
 }
+
 export default App;
