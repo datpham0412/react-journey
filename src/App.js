7fb899c77efd9980d697ffdae24fe631576d1d81
datpham0412 <tiendat041202@gmail.com>
2024-08-16 14:37:47 +1000

    Common hooks: useReducer()

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React, { useState, useCallback } from "react";
-
-function ExpensiveCalculation({ calculate }) {
-  console.log("ExpensiveCalculation component re-rendered");
-  return (
-    <div>
-      <h2>Result of calculation: {calculate()}</h2>
-    </div>
-  );
-}
+import React from "react";
+import Counter from "./rendering/Hooks/CommonHooks/useReducer()/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [otherState, setOtherState] = useState(false);
-
-  // Without useCallback(), this function would be re-created on every render
-  const calculate = useCallback(() => {
-    console.log("Calculating...");
-    return count * 2;
-  }, [count]);
-
   return (
     <div>
-      <h1>Use useCallback() example</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Increment count: {count}
-      </button>
-      <button onClick={() => setOtherState(!otherState)}>
-        Toggle other state
-      </button>
-      <ExpensiveCalculation calculate={calculate} />
+      <Counter />
     </div>
   );
 }
---
+++ src/rendering/Hooks/CommonHooks/useReducer()/Counter.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from "react";
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
+function Counter() {
+  const initialState = { count: 0 };
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
